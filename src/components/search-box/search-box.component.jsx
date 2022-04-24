fd50f2397afbf0f3d731958f004cf006c1e73d6e
KarloDeGuzman <karlo.deguzman.96@gmail.com>
2022-04-24 19:50:15 +0100

    Finished implementing class based components

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,23 @@
+import { Component } from "react";
+
+import "./search-box.styles.css";
+class SearchBox extends Component {
+  constructor(props) {
+    super();
+  }
+  render() {
+    const { onChangeHandler, placeholder, className } = this.props;
+    return (
+      <div>
+        <input
+          className={`search-box ${className}`}
+          type="search"
+          placeholder={placeholder}
+          onChange={onChangeHandler}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
